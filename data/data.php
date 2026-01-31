ec613cc4890d81f98ace662ce9a642551dbfae5d
manoyfinaritra <manoyfinaritra@gmail.com>
2026-01-31 14:08:50 +0300

    verification mot de passe

--- data/data.php
+++ data/data.php
@@ -4,30 +4,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Simulate user data for demonstration purposes
     if (isset($_POST['email']) && isset($_POST['password'])) {
 
-        $sql = "SELECT * FROM users WHERE email = ? AND motdepasse = ?";
+        $sql = "SELECT * FROM users WHERE email = ?";
         $stmt = $connexion->prepare($sql);
-        $stmt->execute([$_POST['email'], $_POST['password']]);
+        $stmt->execute([$_POST['email']]);
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
         
         if ($user) {
-            session_start();
-            $_SESSION["type_admin"] = $user['type_admin'];
+            if (password_verify($_POST['password'], $user['motdepasse'])) {
+                session_start();
+                $_SESSION["type_admin"] = $user['type_admin'];
 
-            if ($_SESSION['type_admin'] == "assistant") {
-                header("location:" . URL . "view/acceuil_admin.php");
-                $_SESSION['nom'] = $user['nom'];
-                $_SESSION['prenom'] = $user['prenom'];
-            }
+                if ($_SESSION['type_admin'] == "assistant") {
+                    header("location:" . URL . "view/acceuil_admin.php");
+                    $_SESSION['nom'] = $user['nom'];
+                    $_SESSION['prenom'] = $user['prenom'];
+                }
 
-            if ($_SESSION['type_admin'] == "enseignant") {
-                $_SESSION['nom'] = $user['nom'];
-                $_SESSION['prenom'] = $user['prenom'];
-                header("location:" . URL . "view/acceuil_enseignant.php");
-            }
-            if ($_SESSION['type_admin'] == "etudiant") {
-                $_SESSION['nom'] = $user['nom'];
-                $_SESSION['prenom'] = $user['prenom'];
-                header("location:" . URL . "view/acceuil_etudiant.php");
+                if ($_SESSION['type_admin'] == "enseignant") {
+                    $_SESSION['nom'] = $user['nom'];
+                    $_SESSION['prenom'] = $user['prenom'];
+                    header("location:" . URL . "view/acceuil_enseignant.php");
+                }
+                if ($_SESSION['type_admin'] == "etudiant") {
+                    $_SESSION['nom'] = $user['nom'];
+                    $_SESSION['prenom'] = $user['prenom'];
+                    header("location:" . URL . "view/acceuil_etudiant.php");
+                }
+            } else {
+                header("location:" . URL . "view/connexion.php?error=motdepasse_incorrect");
             }
         }
     }
